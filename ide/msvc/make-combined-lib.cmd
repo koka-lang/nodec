04165eb824d7e36f7d7e0714292ad8ba09e98e01
REDMOND\kirko <kirko@microsoft.com>
2018-08-17 11:56:54 -0700

    generate single combined library for nodec, as nodecx.lib

---
+++ ide/msvc/make-combined-lib.cmd
@@ -0,0 +1,65 @@
+SETLOCAL ENABLEEXTENSIONS
+@ECHO OFF
+SET PLATFORM=%1
+SET CONFIG=%2
+
+ECHO Platform: %PLATFORM%
+ECHO Configuration: %CONFIG%
+
+SET XPLATFORM=""
+SET XCONFIG=""
+
+IF "%PLATFORM%"=="x64"   SET ZPLATFORM=x64
+IF "%PLATFORM%"=="x64"   SET MPLATFORM=build64
+IF "%PLATFORM%"=="Win32" SET ZPLATFORM=x86
+IF "%PLATFORM%"=="Win32" SET MPLATFORM=build32
+IF "%CONFIG%"=="Debug"   SET ZCONFIG=Debug
+IF "%CONFIG%"=="Release" SET ZCONFIG=ReleaseWithoutAsm
+IF "XCONFIG"=="" GOTO ERROR
+IF "XPLATFORM"=="" GOTO ERROR
+
+SET LIBUV=.\..\..\..\libuv\%CONFIG%\lib\libuv.lib
+IF NOT EXIST %LIBUV% (
+  ECHO %LIBUV% does not exist
+  GOTO ERROR
+)
+
+SET MBEDTLS=.\..\..\..\mbedtls\builds\%MPLATFORM%\library\%CONFIG%\mbedTLS.lib
+IF NOT EXIST %MBEDTLS% (
+  ECHO %MBEDTLS% does not exist
+  GOTO ERROR
+)
+
+SET ZLIB=.\..\..\..\zlib\contrib\vstudio\vc14\%ZPLATFORM%\ZlibStat%ZCONFIG%\zlibstat.lib
+IF NOT EXIST %ZLIB% (
+  ECHO %ZLIB% does not exist
+  GOTO ERROR
+)
+
+SET LIBHANDLER=.\..\..\out\msvc-%PLATFORM%\libhandler\%CONFIG%\libhandler.lib
+IF NOT EXIST %LIBHANDLER% (
+  ECHO %LIBHANDLER% does not exist
+  GOTO ERROR
+)
+
+SET NODEC=.\..\..\out\msvc-%PLATFORM%\nodec\%CONFIG%\nodec.lib
+IF NOT EXIST %NODEC% (
+  ECHO %NODEC% does not exist
+  GOTO ERROR
+)
+
+SET COMBINED_DIR=.\..\..\out\msvc-%PLATFORM%\nodecx\%CONFIG%
+IF NOT EXIST %COMBINED_DIR% (
+  ECHO Creating %COMBINED_DIR%
+  MKDIR %COMBINED_DIR%
+)
+SET COMBINED=%COMBINED_DIR%\nodecx.lib
+@ECHO Creating %COMBINED%
+lib /OUT:%COMBINED%  %LIBUV% %MBEDTLS% %ZLIB% %LIBHANDLER% %NODEC%
+IF NOT ERRORLEVEL 0 GOTO ERROR
+ENDLOCAL
+GOTO END
+:ERROR
+ENDLOCAL
+SET ERRORLEVEL=1
+:END
