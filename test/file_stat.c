c93d87fab8234605032e7d5cfcfd7db995bf8d05
KirkOlynyk <kirko@microsoft.com>
2018-12-20 12:21:57 -0800

    enhanced file_stat to check multiple files

--- test/file_stat.c
+++ test/file_stat.c
@@ -7,12 +7,30 @@
  test file stat
 -----------------------------------------------------------------*/
 
+typedef struct _expected {
+  const char* path;
+  uint64_t mode;
+  uint64_t size;
+} expected_t;
+
+static expected_t const EXPECTED[] = {
+  {"./examples/data/nodec.crt", 33206, 1342},
+};
+
+static bool check_expected(const expected_t* const expt) {
+  nodec_log_debug("stat file %s", expt->path);
+  uv_stat_t stat = async_fs_stat(expt->path);
+  if (stat.st_mode != expt->mode)
+    return false;
+  if (stat.st_size != expt->size)
+    return false;
+  return true;
+}
+
 TEST_IMPL(file_stat) {
-  const char* path = "./examples/data/nodec.crt";
-  nodec_log_debug("stat file %s", path);
-  uv_stat_t stat = async_fs_stat(path);
-  CHECK(stat.st_mode == 33206);
-  CHECK(stat.st_size == 1342);
+  for (size_t i = 0; i < nodec_countof(EXPECTED); i++) {
+    CHECK(check_expected(EXPECTED + i));
+  }
   TEST_IMPL_END;
 }
 
